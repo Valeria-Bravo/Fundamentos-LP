2ed4e036c711613675fe9bb5b57076a26c741b79
Valeria-Bravo <49079491+Valeria-Bravo@users.noreply.github.com>
2021-04-19 21:50:53 -0500

    Add files via upload

---
+++ practica 4/code01-p4.s
@@ -0,0 +1,62 @@
+.data
+	text1: .asciiz "Ingrese un numero: "
+	text2: .asciiz "Los 5 primeros multiplos de "
+	text3: .asciiz " son:"
+	text4: .asciiz "\n"
+	array_1: .word	1, 2, 3, 4, 5
+
+.text
+main:
+	li $v0,4
+	la $a0,	text1
+	syscall
+
+	li $v0,5 
+	syscall
+
+	move $t3, $v0 #4 esta en t3
+	
+	
+	li $v0,4
+	la $a0, text2
+	syscall
+
+	move $a0, $t3
+
+	li $v0,1
+	syscall
+
+	li $v0,4
+	la $a0, text3
+	syscall
+
+	li $t0, 0 # para el array
+    	la $t1, array_1
+
+	
+
+	loop1:
+    		bge     $t0, 5, exit
+    
+    		lw      $t2, 0($t1)
+    		add    $t1, $t1, 4
+
+		mul $t6, $t2, $t3 
+
+		li $v0,4
+		la $a0, text4
+		syscall
+
+    		li      $v0, 1    
+    		move    $a0, $t6 
+    		syscall
+		
+		li      $a0, 32 
+    		li      $v0, 11  
+    		syscall
+    
+    		add    $t0, $t0, 1 
+    		j      loop1
+	exit:
+
+	jr $ra
